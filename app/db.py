4294dd5424b925f1bb420510b6dcbe3c5061c898
DKSASI2003 <dksasi77@gmail.com>
2026-09-15 17:30:08 +0530

    modified app logic

--- app/db.py
+++ app/db.py
@@ -2,20 +2,20 @@ import boto3
 import json
 import psycopg2
 
-client = boto3.client(
-    "secretsmanager",
-    region_name="ap-south-1"
-)
-
-secret = client.get_secret_value(
-    SecretId="8byte-postgres-secret"
-)
-
-creds = json.loads(
-    secret["SecretString"]
-)
-
 def get_connection():
+
+    client = boto3.client(
+        "secretsmanager",
+        region_name="ap-south-1"
+    )
+
+    secret = client.get_secret_value(
+        SecretId="8byte-postgres-secret"
+    )
+
+    creds = json.loads(
+        secret["SecretString"]
+    )
 
     return psycopg2.connect(
         host=creds["host"],
