cbbfe52cccfe5862fafd4d974aeb782786fe9f1d
DKSASI2003 <dksasi77@gmail.com>
2026-09-15 14:45:55 +0530

    fixed unittest

--- app/tests/test_health.py
+++ app/tests/test_health.py
@@ -1,4 +1,4 @@
-from app import main
+from app.app import main
 
 
 def test_health():
--- app/tests/test_home.py
+++ app/tests/test_home.py
@@ -1,5 +1,4 @@
-from app import main
-
+from app.app import main
 def test_home():
 
     client = main.test_client()
